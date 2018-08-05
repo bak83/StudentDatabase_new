91d6e2deb51bad0d431e0a6391b428fc7a707496
Katarzyna Tadaszak <katarzyna.tadaszak@gmail.com>
2018-08-05 13:02:44 +0200

    fixed formating, fixed pesel validation

--- DataBase.cpp
+++ DataBase.cpp
@@ -26,94 +26,92 @@ void DataBase::addNewPerson(Person* r)
 
 bool DataBase::checkPesel(std::string p)
 {
-std::string pesel = p;
-int sum;
-std::array <char, 11> multipliers = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1};
-if (pesel.size()==11){
-    for(int i=0; i < 11; i++)
-    sum += pesel[i] *  multipliers[i];
-    sum %=10;
-}
-else return false;
-if (sum == 0) return true;
-else return false;
-
+    std::string pesel = p;
+    int sum=0;
+    std::array <char, 11> multipliers = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1};
+    if (pesel.size()==11){
+        for(int i=0; i < 11; i++) sum += (pesel[i] *  multipliers[i]);
+        
+        if (sum % 10  == 0) return true;
+        else return false;
+    }
+    else return false;
 }
 
 void DataBase::displayPersonList()
 {
-std::cout << "BAZA DANYCH:" << std::endl;
-for (int i = 0; i < dataBase.size(); i++) {
-    std::cout << i + 1 << ". ";
-    dataBase.at(i)->showAll();
-}
+    std::cout << "BAZA DANYCH:" << std::endl;
+    for (int i = 0; i < dataBase.size(); i++) {
+        std::cout << i + 1 << ". ";
+        dataBase.at(i)->showAll();
+    }
 }
 
 void DataBase::sortStudentsByIndex()
 {
-std::sort(dataBase.begin(), dataBase.end(), [](Person* p1, Person* p2) {
-   return p1->getIndex() < p2->getIndex();
-});
+    std::sort(dataBase.begin(), dataBase.end(), [](Person* p1, Person* p2) {
+        return p1->getIndex() < p2->getIndex();
+    });
 
 }
 
 void DataBase::sortBySurname()
 {
-std::sort(dataBase.begin(), dataBase.end(), [](Person* p1, Person* p2) {
-   return p1->getSurname() < p2->getSurname();
-});
+    std::sort(dataBase.begin(), dataBase.end(), [](Person* p1, Person* p2) {
+        return p1->getSurname() < p2->getSurname();
+    });
 }
 
 void DataBase::sortByPesel()
 {
-std::sort(dataBase.begin(), dataBase.end(), [](Person* p1, Person* p2) {
-   return p1->getPesel() < p2->getPesel();
- });
+    std::sort(dataBase.begin(), dataBase.end(), [](Person* p1, Person* p2) {
+        return p1->getPesel() < p2->getPesel();
+    });
 }
 
 void DataBase::sortBySalary()
 {
-std::sort(dataBase.begin(), dataBase.end(), [](Person* p1, Person* p2)
-{
-    return p1->getSalary() < p2->getSalary();
-});
+    std::sort(dataBase.begin(), dataBase.end(), [](Person* p1, Person* p2){
+        return p1->getSalary() < p2->getSalary();
+    });
 }
 
 
 void DataBase::removeByPesel(std::string pe)
 {
-for (int i = 0; i < dataBase.size(); i++)
-    if (dataBase.at(i)->getPesel() == pe) {
-        dataBase.erase(dataBase.begin()+i);
-        break;
+    for (int i = 0; i < dataBase.size(); i++){
+        if (dataBase.at(i)->getPesel() == pe) {
+            dataBase.erase(dataBase.begin()+i);
+            break;
+        }
     }
 }
 
 
 void DataBase::changeAddressByPesel(std::string pe, std::string n)
-{ std::string newAddress = n;
- for (int i=0; i < dataBase.size(); i++) {
-    if (dataBase.at(i)->getPesel() == pe) {
-        dataBase[i]->setAddress(newAddress);
-        dataBase[i]->getAddress();
-        break;
+{
+    std::string newAddress = n;
+    for (int i=0; i < dataBase.size(); i++) {
+        if (dataBase.at(i)->getPesel() == pe) {
+            dataBase[i]->setAddress(newAddress);
+            dataBase[i]->getAddress();
+            break;
+        }
     }
-}
 }
 
 
 void DataBase::changeSalaryByPesel(std::string pe, float s)
-{ float newSalary = s;
- for (int i = 0; i < dataBase.size(); ++i) {
-    if (dataBase.at(i)->getPesel() == pe) {
-        
-        if ((dataBase.at(i)->getSalary()) >= 0) {
+{ 
+    float newSalary = s;
+    for (int i = 0; i < dataBase.size(); ++i) {
+        if (dataBase.at(i)->getPesel() == pe) {        
+            if ((dataBase.at(i)->getSalary()) >= 0) {
                 dataBase[i]->setSalary(newSalary);
                 dataBase[i]->getSalary();
                 break;
 
              } else dataBase[i]->getSalary();
-
         }
     }
 }
@@ -122,8 +120,7 @@ void DataBase::changeSalaryByPesel(std::string pe, float s)
 void DataBase::saveToFile()
 {
     std::ofstream file;
-    for (auto const & i: dataBase) {
-        
+    for (auto const & i: dataBase) {        
             file << i->getName() << "|"
                  << i->getSurname() << "|"
                  << i->getPesel() << "|"
@@ -131,8 +128,7 @@ void DataBase::saveToFile()
                  << i->getIndex() << "|"
                  << i->getSalary() << "|"
                  << std::endl;
-        }
-
+    }
     file.close();
 }
 /*
@@ -159,81 +155,60 @@ void DataBase::loadFile(std::string fileName)
 
 */
 void DataBase::generateBase(int counter)
-
 {
     std::cout << "BAZA DANYCH LOSOWA" << std::endl;
-   srand (time(NULL));
+    srand (time(NULL));
+    std::fstream file;
 
-   std::fstream file;
-   file.open("name.txt", std::ios::in);
-
-   if (file.good() == false)
-   {
-       std::cout << "File not exist" << std::endl;
-       exit(0);
-   }
+    file.open("name.txt", std::ios::in);
+    if (file.good() == false){
+        std::cout << "File not exist" << std::endl;
+        exit(0);
+    }
     std::vector<std::string> name;
     std::string line;
-
-    while (getline(file,line))
-        name.push_back(line);
+    while (getline(file,line)) name.push_back(line);
     file.close();
 
     file.open("surname.txt", std::ios::in);
-
-    if (file.good() == false)
-    {
+    if (file.good() == false){
         std::cout << "File not exist" << std::endl;
         exit(0);
     }
-     std::vector<std::string> surname;
-     std::string line2;
-
-     while (getline(file,line2))
-         surname.push_back(line2);
-     file.close();
+    std::vector<std::string> surname;
+    std::string line2;
+    while (getline(file,line2)) surname.push_back(line2);
+    file.close();
 
     file.open("PESEL.txt", std::ios::in);
-
-    if (file.good() == false)
-    {
+    if (file.good() == false) {
         std::cout << "File not exist" << std::endl;
         exit(0);
     }
-     std::vector<std::string> PESEL;
-     std::string line3;
+    std::vector<std::string> PESEL;
+    std::string line3;
+    while (getline(file,line3)) PESEL.push_back(line3);
+    file.close();
 
-     while (getline(file,line3))
-         PESEL.push_back(line3);
-     file.close();
-
-     file.open("street.txt", std::ios::in);
-
-     if (file.good() == false)
-     {
+    file.open("street.txt", std::ios::in);
+    if (file.good() == false){
          std::cout << "File not exist" << std::endl;
          exit(0);
      }
-      std::vector<std::string> street;
-      std::string line4;
+    std::vector<std::string> street;
+    std::string line4;
+    while (getline(file,line4)) street.push_back(line4);
+    file.close();
 
-      while (getline(file,line4))
-          street.push_back(line4);
-      file.close();
-
-      file.open("city.txt", std::ios::in);
-
-      if (file.good() == false)
-      {
+    file.open("city.txt", std::ios::in);
+    if (file.good() == false){
           std::cout << "File not exist" << std::endl;
           exit(0);
-      }
-       std::vector<std::string> city;
-       std::string line5;
-
-       while (getline(file,line5))
-           city.push_back(line5);
-       file.close();
+    }
+    std::vector<std::string> city;
+    std::string line5;
+    while (getline(file,line5)) city.push_back(line5);
+    file.close();
 
     for (int i = 0;i < counter;i++)
     std::cout << i+1 << ". " << surname[(std::rand() % 85)+1] << " " << name[(std::rand() % 500)+1]
