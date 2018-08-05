36d2c6f971f4d378431692829669abd6bc8823cd
Katarzyna Tadaszak <katarzyna.tadaszak@gmail.com>
2018-08-05 02:32:32 +0200

    minor changes

--- DataBase.cpp
+++ DataBase.cpp
@@ -8,7 +8,7 @@
 #include <sstream>
 #include <ctime>
 #include <stdexcept>
-
+#include <array>
 
 
 void DataBase::addNewPerson(Person* r)
@@ -19,93 +19,95 @@ void DataBase::addNewPerson(Person* r)
         else {throw std::invalid_argument("Invalid PESEL number ");}
     }catch(std::invalid_argument& e)
         {
-            std::cout << e.what() << std::endl;
-        }
-
+       std::cout << e.what() << std::endl;
     }
+
+}
 
 bool DataBase::checkPesel(std::string p)
 {
-    std::string pesel = p;
-    int sum;
-    if (pesel.size()==11){
-        sum = pesel[0] * 1 + pesel[1] * 3 + pesel[2] * 7 + pesel[3] * 9 + pesel[4] + pesel[5] * 3 + pesel[6] * 7 + pesel[7] * 9 + pesel[8] + pesel[9] * 3 + pesel[10];
-        sum %=10;
-    }
-    else return false;
-    if (sum == 0) return true;
-    else return false;
+std::string pesel = p;
+int sum;
+std::array <char, 11> multipliers = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1};
+if (pesel.size()==11){
+    for(int i=0; i < 11; i++)
+    sum += pesel[i] *  multipliers[i];
+    sum %=10;
+}
+else return false;
+if (sum == 0) return true;
+else return false;
 
 }
 
 void DataBase::displayPersonList()
 {
-    std::cout << "BAZA DANYCH:" << std::endl;
-    for (int i = 0; i < dataBase.size(); ++i) {
-        std::cout << i+1 << ". ";
-        dataBase.at(i)->showAll();
-    }
+std::cout << "BAZA DANYCH:" << std::endl;
+for (int i = 0; i < dataBase.size(); i++) {
+    std::cout << i + 1 << ". ";
+    dataBase.at(i)->showAll();
+}
 }
 
 void DataBase::sortStudentsByIndex()
 {
-    std::sort(dataBase.begin(), dataBase.end(), [](Person* p1, Person* p2) {
-       return p1->getIndex() < p2->getIndex();
-    });
+std::sort(dataBase.begin(), dataBase.end(), [](Person* p1, Person* p2) {
+   return p1->getIndex() < p2->getIndex();
+});
 
 }
 
 void DataBase::sortBySurname()
 {
-    std::sort(dataBase.begin(), dataBase.end(), [](Person* p1, Person* p2) {
-       return p1->getSurname() < p2->getSurname();
-    });
+std::sort(dataBase.begin(), dataBase.end(), [](Person* p1, Person* p2) {
+   return p1->getSurname() < p2->getSurname();
+});
 }
 
 void DataBase::sortByPesel()
 {
-    std::sort(dataBase.begin(), dataBase.end(), [](Person* p1, Person* p2) {
-       return p1->getPesel() < p2->getPesel();
-     });
+std::sort(dataBase.begin(), dataBase.end(), [](Person* p1, Person* p2) {
+   return p1->getPesel() < p2->getPesel();
+ });
 }
 
 void DataBase::sortBySalary()
 {
-    std::sort(dataBase.begin(), dataBase.end(), [](Person* p1, Person* p2)
-    {
-        return p1->getSalary() < p2->getSalary();
-    });
+std::sort(dataBase.begin(), dataBase.end(), [](Person* p1, Person* p2)
+{
+    return p1->getSalary() < p2->getSalary();
+});
 }
 
 
 void DataBase::removeByPesel(std::string pe)
 {
-    for (int i = 0; i < dataBase.size(); ++i) {
-        if (dataBase.at(i)->getPesel() == pe) {
-            dataBase.erase(dataBase.begin()+i);
-            break;
-        }
+for (int i = 0; i < dataBase.size(); i++)
+    if (dataBase.at(i)->getPesel() == pe) {
+        dataBase.erase(dataBase.begin()+i);
+        break;
     }
 }
 
+
 void DataBase::changeAddressByPesel(std::string pe, std::string n)
 { std::string newAddress = n;
-     for (int i = 0; i < dataBase.size(); ++i) {
-        if (dataBase.at(i)->getPesel() == pe) {
-            dataBase[i]->setAddress(newAddress);
-            dataBase[i]->getAddress();
-            break;
-        }
+ for (int i=0; i < dataBase.size(); i++) {
+    if (dataBase.at(i)->getPesel() == pe) {
+        dataBase[i]->setAddress(newAddress);
+        dataBase[i]->getAddress();
+        break;
     }
+}
 }
 
 
 void DataBase::changeSalaryByPesel(std::string pe, float s)
 { float newSalary = s;
-     for (int i = 0; i < dataBase.size(); ++i) {
-        if (dataBase.at(i)->getPesel() == pe) {
-            
-            if ((dataBase.at(i)->getSalary()) >= 0) {
+ for (int i = 0; i < dataBase.size(); ++i) {
+    if (dataBase.at(i)->getPesel() == pe) {
+        
+        if ((dataBase.at(i)->getSalary()) >= 0) {
                 dataBase[i]->setSalary(newSalary);
                 dataBase[i]->getSalary();
                 break;
@@ -119,47 +121,36 @@ void DataBase::changeSalaryByPesel(std::string pe, float s)
 
 void DataBase::saveToFile()
 {
-    std::fstream file;
-    file.open("Base.txt", std::ios::out);
- if(!file.is_open())
-     throw std::runtime_error("Unable to open");
- else{
-    for (int i = 0; i < dataBase.size(); ++i) {
+    std::ofstream file;
+    for (auto const & i: dataBase) {
         
-            file << dataBase[i]->getName() << "|"
-                 << dataBase[i]->getSurname() << "|"
-                 << dataBase[i]->getPesel() << "|"
-                 << dataBase[i]->getAddress() << "|"
-                 << dataBase[i]->getIndex() << "|"
-                 << dataBase[i]->getSalary() << "|"
+            file << i->getName() << "|"
+                 << i->getSurname() << "|"
+                 << i->getPesel() << "|"
+                 << i->getAddress() << "|"
+                 << i->getIndex() << "|"
+                 << i->getSalary() << "|"
                  << std::endl;
         }
 
     file.close();
-    }
 }
 /*
-void DataBase::loadFile()
+void DataBase::loadFile(std::string fileName)
 {
-    std::fstream file;
-    file.open("Base.txt", std::ios::in);
-    file.open("Base.txt", std::ios::out);
-    if(!file.good())
-         throw std::runtime_error("Unable to open");
-    else{
-        std::string line;
-        std::string indivString;
-        char separator = '|';
-        std::vector<std::string> strVec;
+    std::ifstream file (fileName);
+    std::string line;
+    char separator = '|';
 
     while (std::getline(file, line)) {
-        std::stringstream ss(line);
-        while (std::getline(ss, indivString, separator)) {
-            strVec.push_back(indivString);
+        if(line.find("-1") != string::npos)
+        {
+            *Person person = new Student(line);
+            addNewPerson(person);
+
+
         }
-        Person* Ptr = new Person(strVec[0], strVec[1], strVec[2], strVec[3], strVec[4], strVec[5]);
-        addNewPerson(Ptr);
-        strVec.clear();
+
     }
 
     file.close();
